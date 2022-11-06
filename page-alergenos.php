086931fc6b6d133b6c9bf1a841e71e27529e0c5e
Jorge Garcia Cano <garciacano.jrg@gmail.com>
2022-11-06 15:25:37 +0100

    added allergens page

--- page-alergenos.php
+++ page-alergenos.php
@@ -1,64 +1,45 @@
-<?php get_header(); ?>
-    <main class="pagina seccion no-sidebars contenedor">
-    <?php
-
-    the_content();
-
-    get_alergenos(true);
-
-    ?>
-    <div class="page-alergenos-productos-list">
-    <?php
-    $args = array(
-                'post_type'=>'product'
-            );
+<?php get_header(); 
+global $post;
+$featured_image_url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+?>
+        <div style="margin-top: 30px" class="site-content shop-content-area col-lg-12 col-12 col-md-12 description-area-before content-with-products">
+            <div class="d-flex flex-wrap">
+            <?php $args = array('post_type'=>'product');
 
             $productos = new WP_Query($args);
 
             while($productos->have_posts()) : $productos->the_post();
 
-            ?>
+                ?>
 
-            <div class='page-alergenos-item' product-id='<?php echo the_title(); ?>'>
+                <div class='product-allergens-details' product-id='<?php echo the_ID(); ?>'>
 
-            <div class="page-alergenos-item__image">
-                <a href="<?php the_permalink() ?>">
-                    <?php the_post_thumbnail(); ?>
-                    <h4><?php the_title(); ?> </h4>
+                    <div class="image-container"> <?php the_post_thumbnail('thumbnail'); ?> </div>
+                    <div class="data-container">
+                        <h4><?php the_title(); ?> </h4>
+                        <div>
+                            <ul>
+                            <?php
+                            foreach(get_post_meta($productos->post->ID, "lista_alergenos") as $value) {
+                                foreach($value as $alergeno) {
+                                    echo "<li>".$alergeno."</li>";
+                                }
+                            }
+                            ?>
+                            </ul>
+                        </div>
+                        <?php
+                        $atributos = $product->get_attributes(); ?>
+                        <div>
+                            <?php
+                        if ($atributos !== null && isset($atributos['sin-lactosa'])) {
+                            echo "<span class='attribute'><span style='color:red'>*</span> Disponible sin lactosa</span>";
+                        } ?>
+                        </div>
+                    </div>
+                </div>
 
-                    <?php
-
-                    $atributos = get_post_meta( $productos->post->ID, "_default_attributes");
-
-                    if (isset($atributos[0])) {
-                        
-                        foreach($atributos[0] as $key => $i) {
-                            if (substr_count($key, 'sin-lactosa') > 0) {
-                                echo "<span class='attribute'>* Disponible sin lactosa</span>";
-                            } 
-                        }
-                    }
-
-                    ?>
-                </a>
-            </div>
-
-            <div class="page-alergenos-item__alergenos">
-            <?php
-                foreach(get_post_meta($productos->post->ID, "lista_alergenos") as $value) {
-                    foreach($value as $alergeno) {
-                        echo "<input type='hidden' placeholder='$alergeno' value='$alergeno'></input>";
-                    }
-                }
-            ?>
-            </div>
-            </div>
-
-            <?php
-
-            endwhile; wp_reset_postdata();
-
-            ?>
-            </div>
-    </main>
+                <?php endwhile; wp_reset_postdata(); ?>
+                    </div>
+        </div>
 <?php get_footer(); ?>
