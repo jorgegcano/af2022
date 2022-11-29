e7b87513a9977f783082e03d4a74c5f15c74352b
Jorge Garcia Cano <garciacano.jrg@gmail.com>
2022-11-29 18:40:26 +0100

    disable heartbeat

--- functions.php
+++ functions.php
@@ -396,3 +396,11 @@ function bbloomer_product_add_on_display_emails( $fields ) {
     $fields['dedication_text_add_on'] = 'Dedicatoria del cliente';
     return $fields; 
 }
+
+add_action( 'init', 'my_deregister_heartbeat', 1 );
+function my_deregister_heartbeat() {
+	global $pagenow;
+
+	if ( 'post.php' != $pagenow && 'post-new.php' != $pagenow )
+		wp_deregister_script('heartbeat');
+}
